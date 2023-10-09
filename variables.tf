1ed9d1f1256bcc37dc320bf8151801d56b0853b0
Sri Harsha Peri <peri.s@northeastern.edu>
2023-10-08 23:10:51 -0400

    changed gcp infra

--- variables.tf
+++ variables.tf
@@ -2,14 +2,10 @@ variable "default_region" {
   description = "gcp organization project default region"
 }
 
-variable "project_name" {
-  description = "gcp organization project name"
-}
-
 variable "project_id" {
   description = "gcp organization project id"
 }
 
-# variable "service_account_credentials" {
-#   description = "gcp service accountr credentials"
-# }
+variable "service_account_credentials" {
+  description = "gcp service account credentials"
+}

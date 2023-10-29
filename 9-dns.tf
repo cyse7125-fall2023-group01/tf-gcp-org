432a62af2439409aacb5d214c06a09fa726870c6
Abhilash <gade.ab@northeastern.edu>
2023-10-28 23:02:26 -0400

    fix(updated): updated gke config

--- 9-dns.tf
+++
@@ -1,7 +0,0 @@
-resource "google_dns_record_set" "frontend" {
-  name         = "${var.gcp_dev_hosted_zone}."
-  type         = "A"
-  ttl          = 60
-  managed_zone = "dev-subdomain-csye-gcp"
-  rrdatas      = [google_compute_address.csye_7125_nat_compute_address.address]
-}
--- variables.tf
+++ variables.tf
@@ -1,42 +1,42 @@
-variable "default_region" {
-  description = "gcp organization project default region"
-}
-
-variable "project_id" {
-  description = "gcp organization project id"
-}
-
-variable "service_account_credentials" {
-  description = "gcp service account credentials"
-}
-
-variable "machine_type" {
-  description = "type of compute instance"
-}
-
-variable "gcp_dev_hosted_zone" {
-  description = "gcp dev hosted zone domain name"
-}
-
-variable "machine_image" {
-  description = "type of machine image used for compute instance"
-}
-
-variable "service_account_email" {
-  description = "service account email"
-}
-
-variable "compute_instance_availability_zone" {
-  description = "availability zone in the configured region for compute instance"
-}
-
-variable "cluster_name" {
+variable "project" {
+  description = "The Google Cloud project ID."
   type        = string
-  description = "The name of the cluster"
-  default     = "webapp-private-cluster"
 }
 
-# variable "bastion_members" {
-#   type        = list(string)
-#   description = "List of users, groups, SAs who need access to the bastion host"
-# }
+variable "filename" {
+  description = "Path to the Google Cloud service account key JSON file."
+  type        = string
+  default     = "gcp_sa_key.json"
+}
+
+variable "region" {
+  description = "The Google Cloud region for resources."
+  type        = string
+}
+
+variable "subnet_public_ip_cidr" {
+  description = "IP CIDR range for the private subnetwork."
+  type        = string
+}
+
+variable "subnet_private_ip_cidr" {
+  description = "IP CIDR range for the private subnetwork."
+  type        = string
+}
+
+variable "ip_cidr_range_private" {
+  description = "IP CIDR range for the public subnetwork."
+  type        = string
+}
+
+variable "bastion_machine_type" {
+  description = "Machine type for the bastion instance."
+  type        = string
+}
+
+variable "bastion_zone" {
+  description = "Zone for the bastion instance."
+  type        = string
+}
+
+
